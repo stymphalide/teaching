77f4df86f2f6697c89855fddfc5320f3a92b2976
stymphalide <angelo.birrer@gmail.com>
2018-01-27 17:41:50 +0100

    create_set function done.

--- sofia/develop/python/exercises/lesson_1/set_simulation.py
+++ sofia/develop/python/exercises/lesson_1/set_simulation.py
@@ -27,9 +27,22 @@ def is_equal_or_different(card_one,card_two,card_three,feature):
 	test_features_diffrent = card_one[feature] != card_two[feature] and card_one[feature] != card_three[feature]  and card_two[feature] != card_three[feature]
 	return test_features_equal or test_features_diffrent	
 
+def create_set(card_one, card_two):
+	card_three = {}
+	card_three["number"] = create_feature(list(numbers), card_one["number"], card_two["number"])
+	card_three["shape"] = create_feature(list(shapes), card_one["shape"], card_two["shape"])
+	card_three["filling"] = create_feature(list(fillings), card_one["filling"], card_two["filling"])
+	card_three["colour"] = create_feature(list(colours), card_one["colour"], card_two["colour"])
+	return card_three
 
 
-
+def create_feature(features, feature1, feature2):
+	if feature1 == feature2:
+		return feature1
+	else:
+		features.remove(feature1)
+		features.remove(feature2)
+		return features[0]
 
 print(is_set(card_one, card_two, card_three))
 
